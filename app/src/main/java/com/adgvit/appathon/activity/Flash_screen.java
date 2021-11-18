a238f3e2b4e2b2ef5f9cd3b05ff4b14bcbf04735
Diablo-bit <anshgoel01@gmail.com>
2021-11-18 12:30:13 +0530

    splash screen added

---
+++ app/src/main/java/com/adgvit/appathon/activity/Flash_screen.java
@@ -0,0 +1,39 @@
+package com.adgvit.appathon.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.adgvit.appathon.R;
+
+public class Flash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_screen);
+
+
+        Thread thread = new Thread(){
+
+            public void run(){
+                try{
+                    sleep(4000);
+
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+
+                }
+                finally {
+                    Intent intent = new Intent(Flash_screen.this , MainActivity.class);
+                    startActivity(intent);
+
+                }
+                }
+
+        };thread.start();
+    }
+}
