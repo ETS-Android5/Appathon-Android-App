09fa315c7e56d84bf4d955287a3b33d08d075b9d
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2021-11-25 11:21:35 +0530

    Made some changes

--- app/src/main/java/com/adgvit/appathon/activity/MainActivity.java
+++ app/src/main/java/com/adgvit/appathon/activity/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
@@ -44,13 +45,11 @@ public class MainActivity extends AppCompatActivity {
         String token = sp.getString("token", "");
         if (token.equals("")){
             editor.putString("token", "token");
-            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
-            bottomSheetDialog.setContentView(R.layout.welcome_page);
-            bottomSheetDialog.setCanceledOnTouchOutside(true);
+            Intent i = new Intent(MainActivity.this,WelcomeActivity.class);
+            startActivity(i);
 
             //BottomSheetBehavior.from(bottomSheetDialog.findViewById(R.id.)).setDraggable(false);
 
-            bottomSheetDialog.show();
         }
 //        Start.setOnClickListener(new View.OnClickListener() {
 //            @Override
---
+++ app/src/main/java/com/adgvit/appathon/activity/WelcomeActivity.java
@@ -0,0 +1,32 @@
+package com.adgvit.appathon.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.adgvit.appathon.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        ImageView getStarted = findViewById(R.id.startbtn);
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        finish();
+    }
+}
